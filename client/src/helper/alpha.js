db9e5acad7f42b1b8f7d694d29a17deafe3ebb70
Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
2021-02-21 01:13:20 +0530

    feat: blockchain helper

---
+++ client/src/helper/alpha.js
@@ -0,0 +1,10 @@
+const conseiljs = require("conseiljs");
+
+module.exports = {
+  publicKey: "edpkuxJDCtWQLsi6JzQSaMZwJei2KZNDRoTHkst27as9kmh2hPVQNy",
+  privateKey:
+    "edskRusSwtc1hUzkYSKrShyXafBZmcw38N4ZxcQGrhwpVfeqcuDKVEwUn1tqh1CH5YKn8n9nFCWL8qpuwknjo2MFyhHBCEZgM6",
+  publicKeyHash: "tz1RGoWgci6h21xyEgfPJU9qkK4gpThiDN4n",
+  seed: "",
+  storeType: conseiljs.StoreType.Fundraiser,
+};
---
+++ client/src/pages/BlockTester.jsx
@@ -0,0 +1,54 @@
+import { Button, TextField } from "@material-ui/core";
+import React from "react";
+import BlockChainHelper from "../helper/blockchainHelper";
+
+export default function BlockTester() {
+    const blockHelper = new BlockChainHelper();
+
+    const transferToken = async (amount, fromAddr, toAddr) => {
+        const result = await blockHelper.transferToken(
+            amount,
+            fromAddr,
+            toAddr
+        );
+        if (result.success) {
+            alert("Major GG!");
+        } else {
+            alert("EPIC FAIL BLOCKCHAIN LMAO!");
+        }
+    };
+
+    const mintToken = async (amount, fromAddr) => {
+        const result = await blockHelper.mintToken(amount, fromAddr);
+        if (result.success) {
+            alert("Major GG!");
+        } else {
+            alert("EPIC FAIL BLOCKCHAIN LMAO!");
+        }
+    };
+
+    return (
+        <div>
+            <form noValidate autoComplete="off">
+                <TextField id="standard-basic" label="Standard" />
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                />
+            </form>
+            <Button
+                onClick={transferToken(
+                    1,
+                    "tz1RGoWgci6h21xyEgfPJU9qkK4gpThiDN4n",
+                    "tz1eViSEkLDYcnf83ztwoFCfuiiGkHwxgzWQ"
+                )}
+                variant="contained"
+                color="primary"
+            >
+                Transfer Tokens
+            </Button>
+        </div>
+    );
+}
